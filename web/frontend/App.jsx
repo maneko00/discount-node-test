a1232574fbed5aaf61291647a0f371982833511a
maneko00 <trueneko1@gmail.com>
2022-07-17 08:50:15 +0900

    割引の作成

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -1,35 +1,38 @@
-import { BrowserRouter } from "react-router-dom";
-import { NavigationMenu } from "@shopify/app-bridge-react";
-import Routes from "./Routes";
+import { BrowserRouter } from 'react-router-dom'
+import { NavigationMenu } from '@shopify/app-bridge-react'
+import Routes from './Routes'
 
 import {
   AppBridgeProvider,
+  DiscountProvider,
   QueryProvider,
   PolarisProvider,
-} from "./components";
+} from './components'
 
 export default function App() {
   // Any .tsx or .jsx files in /pages will become a route
-  // See documentation for <Routes /> for more info
-  const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
+  // For more information, refer to the <Routes /> documentation
+  const pages = import.meta.globEager('./pages/**/!(*.test.[jt]sx)*.([jt]sx)')
 
   return (
     <PolarisProvider>
       <BrowserRouter>
         <AppBridgeProvider>
-          <QueryProvider>
-            <NavigationMenu
-              navigationLinks={[
-                {
-                  label: "Page name",
-                  destination: "/pagename",
-                },
-              ]}
-            />
-            <Routes pages={pages} />
-          </QueryProvider>
+          <DiscountProvider>
+            <QueryProvider>
+              <NavigationMenu
+                navigationLinks={[
+                  {
+                    label: 'New volume discount',
+                    destination: '/Volume/new'
+                  }
+                ]}
+              />
+              <Routes pages={pages} />
+            </QueryProvider>
+          </DiscountProvider>
         </AppBridgeProvider>
       </BrowserRouter>
     </PolarisProvider>
-  );
+  )
 }
